675946e02066726656b3448ab083c9f6fbbf835b
andrew-oyanguren <aaoyanguren@outlook.com>
2024-01-25 14:48:04 -0800

    Added programmatic naviagtion to home page from error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,9 +1,15 @@
-import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+import {
+  useRouteError,
+  isRouteErrorResponse,
+  useNavigate,
+} from 'react-router-dom';
 import { Button } from '../../components';
 
 export default function ErrorPage() {
   let status = 404;
   let statusText = 'Page Not Found!';
+
+  const navigate = useNavigate();
 
   const error = useRouteError();
 
@@ -17,8 +23,8 @@ export default function ErrorPage() {
       <h1>{status}</h1>
       <p>{statusText}</p>
       <Button
-        action={() => console.log('Navigate Home')}
-        size='LARGE'
+        action={() => navigate('/')}
+        size='MEDIUM'
         text='Home Page'
         primary
       />
